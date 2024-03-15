6db9b01cf5d29052756286512e496c37af9f14c5
mmk <mmkkarbalaei@gmail.com>
2024-03-15 16:08:20 +0330

    migration product

---
+++ database/migrations/2024_03_15_123126_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+
+            $table->foreignId('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->string('slug')->unique();
+            $table->string("primary_image");
+            $table->text('description');
+            $table->integer('status')->default(1);
+            $table->boolean('is_active')->default(1);
+            $table->unsignedInteger('delivery_amount')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
